a4cc3adbcda29140a22761f8299df4726120fd33
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-07-31 21:58:33 +0530

    [Ariprasath] added: Welcome note to the user registration program

---
+++ MainMethod.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegUsingAnnotationsReflections
+{
+    class MainMethod
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User registration using Annotations for checking the patterns and reflections for generating meta data");
+        }
+    }
+}
